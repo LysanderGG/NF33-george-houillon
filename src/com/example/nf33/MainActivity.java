08ac67a9d0d8e57760a8c4fc2a3975d2244f5c81
Nelson HOUILLON <houillon.nelson@gmail.com>
2013-04-08 17:20:04 +0200

    affichage temps-réel des 3 axes
    Signed-off-by: Nelson HOUILLON <houillon.nelson@gmail.com>

--- src/com/example/nf33/MainActivity.java
+++ src/com/example/nf33/MainActivity.java
@@ -1,15 +1,39 @@
 package com.example.nf33;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.hardware.SensorManager;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+	private Sensor sensor;
+	private TextView tv_axis_x,
+	                 tv_axis_y,
+	                 tv_axis_z;
+
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		tv_axis_x = (TextView)findViewById(R.id.tv_axis_x);
+		tv_axis_y = (TextView)findViewById(R.id.tv_axis_y);
+		tv_axis_z = (TextView)findViewById(R.id.tv_axis_z);
+
+		sensor = new Sensor(this);
+		SensorManager m = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+		m.registerListener(sensor, SensorManager.SENSOR_ACCELEROMETER);
+	}
+
+	void handleMeasure(float x, float y, float z)
+	{
+		tv_axis_x.setText(getString(R.string.axis_x) + x);
+		tv_axis_y.setText(getString(R.string.axis_y) + y);
+		tv_axis_z.setText(getString(R.string.axis_z) + z);
 	}
 
 	@Override
---
+++ src/com/example/nf33/Sensor.java
@@ -0,0 +1,26 @@
+package com.example.nf33;
+
+import android.hardware.SensorListener;
+
+public class Sensor implements SensorListener {
+	float x, y, z;
+
+	private final MainActivity parent_activity;
+
+	Sensor(MainActivity a)
+	{
+		parent_activity = a;
+	}
+
+	@Override
+	public void onAccuracyChanged(int arg0, int arg1) {}
+
+	@Override
+	public void onSensorChanged(int arg0, float[] arg1) {
+		x = arg1[0];
+		y = arg1[1];
+		z = arg1[2];
+		parent_activity.handleMeasure(x, y, z);
+	}
+
+}
